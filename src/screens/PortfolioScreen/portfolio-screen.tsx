67efdb88fcb5036019b39fe9bdd17054b5ef6954
Khachatur <k.arutyunyan@devcontour.ru>
2024-11-25 02:23:08 +0400

    changes

--- src/screens/PortfolioScreen/portfolio-screen.tsx
+++ src/screens/PortfolioScreen/portfolio-screen.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useRef } from 'react';
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Carousel } from 'react-responsive-carousel';
+import { Mousewheel, FreeMode } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 import Header from '../../components/header';
-import PortfolioElem from '../../components/portfolio-elem';
 import Form from '../../components/form';
 
 import VtbIcon from './icons/vtb-min.svg';
@@ -16,13 +15,48 @@ import TelekomIcon from './icons/telecom.svg';
 import LuxyIcon from './icons/luxxy.svg';
 import MamadoIcon from './icons/mamado.svg';
 import SverlodIcon from './icons/sverlod.svg';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/mousewheel';
+
 
 const PortfolioScreen: React.FC = () => {
   const [isVisibleForm, setIsVisibleForm] = useState(false);
   const [activeLogo, setActiveLogo] = useState<any>(null);
-  const scrollContainerRef = useRef<any>(null);
-  const [images, setImages] = useState([
+  const swiperRef = useRef<any>(null);
+
+
+  const images = [
+
+    {
+      uri: require('../../assets/images/tellekom.png'),
+      description: 'Подробнее',
+      screen: '',
+      title: 'Таттелеком',
+      color: '#FFFFFF',
+      size: { width: '72px', height: '18px' },
+      id: 7
+    },
+    {
+      uri: require('../../assets/images/luxxy.png'),
+      description: 'Подробнее',
+      screen: '',
+      title: 'Luxxxy',
+      color: '#FFFFFF',
+      size: { width: '72px', height: '18px' },
+      id: 124
+    },
+    {
+      uri: require('../../assets/images/mamado.png'),
+      description: 'Подробнее',
+      screen: '',
+      title: 'Mamado',
+      color: '#FFFFFF',
+      size: { width: '28px', height: '28px' },
+      id: 123
+    },
     {
       uri: require('../../assets/images/portfolio1.png'),
       description: 'Подробнее',
@@ -54,82 +88,204 @@ const PortfolioScreen: React.FC = () => {
       id: 3
     },
     {
+      uri: require('../../assets/images/utgServices.png'),
+      description: 'Подробнее',
+      screen: '',
+      title: 'UTG Услуги',
+      color: '#FFFFFF',
+      size: { width: '81px', height: '20px' },
+      id: 4
+    },
+    {
       uri: require('../../assets/images/portfolio3.png'),
       description: 'Подробнее',
       screen: '/utg-support',
       title: 'UTG поддержка корпоративных сайтов\nи клиентских сервисов',
       color: '#FFFFFF',
+      size: { width: '81px', height: '20px' },
+      id: 5
+    },
+    {
+      uri: require('../../assets/images/nespresso.png'),
+      description: 'Подробнее',
+      screen: '',
+      title: 'Nespresso',
+      color: '#FFFFFF',
+      icon: NesspressoIcon,
+      size: { width: '100px', height: '20px' },
+      id: 6
+    },
+    {
+      uri: require('../../assets/images/tellekom.png'),
+      description: 'Подробнее',
+      screen: '',
+      title: 'Таттелеком',
+      color: '#FFFFFF',
+      icon: TelekomIcon,
+      size: { width: '72px', height: '18px' },
+      id: 7
+    },
+    {
+      uri: require('../../assets/images/luxxy.png'),
+      description: 'Подробнее',
+      screen: '',
+      title: 'Luxxxy',
+      color: '#FFFFFF',
+      icon: LuxyIcon,
+      size: { width: '72px', height: '18px' },
+      id: 8
+    },
+    {
+      uri: require('../../assets/images/mamado.png'),
+      description: 'Подробнее',
+      screen: '',
+      title: 'Mamado',
+      color: '#FFFFFF',
+      icon: MamadoIcon,
+      size: { width: '28px', height: '28px' },
+      id: 9
+    },
+    {
+      uri: require('../../assets/images/svarlodskaya.png'),
+      description: 'Подробнее',
+      screen: '',
+      title: 'Свердловская киностудия',
+      color: '#FFFFFF',
       icon: SverlodIcon,
+      size: { width: '81px', height: '28px' },
+      id: 10
+    }
+  ];
+
+  const images2 = [
+    {
+      uri: require('../../assets/images/portfolio1.png'),
+      description: 'Подробнее',
+      screen: '/vtb-detail',
+      title: 'ВТБ',
+      color: '#303030',
+      icon: VtbIcon,
+      size: { width: '56px', height: '20px' },
+      id: 1
+    },
+    {
+      uri: require('../../assets/images/portfolio6.png'),
+      description: 'Подробнее',
+      screen: '/cowberry-detail',
+      title: 'Брусника',
+      color: '#FFFFFF',
+      icon: CowberryIcon,
+      size: { width: '128px', height: '20px' },
+      id: 2
+    },
+    {
+      uri: require('../../assets/images/portfolio2.png'),
+      description: 'Подробнее',
+      screen: '/utg-detail',
+      title: 'UTG Учебный центр',
+      color: '#FFFFFF',
+      icon: UtgAviaIcon,
+      size: { width: '51px', height: '20px' },
+      id: 3
+    },
+    {
+      uri: require('../../assets/images/utgServices.png'),
+      description: 'Подробнее',
+      screen: '',
+      title: 'UTG Услуги',
+      color: '#FFFFFF',
       size: { width: '81px', height: '20px' },
       id: 4
     },
-  ]);
+    {
+      uri: require('../../assets/images/portfolio3.png'),
+      description: 'Подробнее',
+      screen: '/utg-support',
+      title: 'UTG поддержка корпоративных сайтов\nи клиентских сервисов',
+      color: '#FFFFFF',
+      size: { width: '81px', height: '20px' },
+      id: 5
+    },
+    {
+      uri: require('../../assets/images/nespresso.png'),
+      description: 'Подробнее',
+      screen: '',
+      title: 'Nespresso',
+      color: '#FFFFFF',
+      icon: NesspressoIcon,
+      size: { width: '100px', height: '20px' },
+      id: 6
+    },
+    {
+      uri: require('../../assets/images/tellekom.png'),
+      description: 'Подробнее',
+      screen: '',
+      title: 'Таттелеком',
+      color: '#FFFFFF',
+      icon: TelekomIcon,
+      size: { width: '72px', height: '18px' },
+      id: 7
+    },
+    {
+      uri: require('../../assets/images/luxxy.png'),
+      description: 'Подробнее',
+      screen: '',
+      title: 'Luxxxy',
+      color: '#FFFFFF',
+      icon: LuxyIcon,
+      size: { width: '72px', height: '18px' },
+      id: 8
+    },
+    {
+      uri: require('../../assets/images/mamado.png'),
+      description: 'Подробнее',
+      screen: '',
+      title: 'Mamado',
+      color: '#FFFFFF',
+      icon: MamadoIcon,
+      size: { width: '28px', height: '28px' },
+      id: 9
+    },
+    {
+      uri: require('../../assets/images/svarlodskaya.png'),
+      description: 'Подробнее',
+      screen: '',
+      title: 'Свердловская киностудия',
+      color: '#FFFFFF',
+      icon: SverlodIcon,
+      size: { width: '81px', height: '28px' },
+      id: 10
+    }
+  ];
 
   const imageRefs = useRef<HTMLDivElement[]>([]);
 
-
-  const data = [...images, ...images, ...images];
-
-  const handleWheel = (e: any) => {
-    if (scrollContainerRef.current) {
-      e.preventDefault();
-      scrollContainerRef.current.scrollLeft += e.deltaY;
-    }
-  };
-
-
   const handleIconClick = (index: number) => {
-    const element = imageRefs.current[index];
-    const container = scrollContainerRef.current;
-    if (!element || !container) return;
-
-    const elementRect = element.getBoundingClientRect();
-    const containerRect = container.getBoundingClientRect();
-
-    // Проверяем, виден ли элемент полностью
-    const isFullyVisible =
-      elementRect.left >= containerRect.left && elementRect.right <= containerRect.right;
-
-    if (!isFullyVisible) {
-      // Скроллим элемент в центр
-      element.scrollIntoView({
-        behavior: 'smooth',
-        block: 'nearest',
-        inline: 'center',
-      });
+    if (swiperRef.current) {
+      swiperRef.current.slideToLoop(index);
     }
   };
 
   useEffect(() => {
-    const element = imageRefs.current[1];
-    if (element) {
-      element.scrollIntoView({ block: 'center', inline: 'center' });
-      document.body.scrollTo({ top: 0 });
+    const syncActiveIndex = () => {
+      if (swiperRef.current) {
+        setActiveLogo(swiperRef.current.realIndex);
+      }
+    };
+
+    if (swiperRef.current) {
+      swiperRef.current.on('slideChange', syncActiveIndex);
     }
+
+    return () => {
+      if (swiperRef.current) {
+        swiperRef.current.off('slideChange', syncActiveIndex);
+      }
+    };
   }, []);
 
-  const handleElementClick = (index: number) => {
-    const element = imageRefs.current[index];
-    const container = scrollContainerRef.current;
-    if (!element || !container) return;
 
-    const elementRect = element.getBoundingClientRect();
-    const containerRect = container.getBoundingClientRect();
 
-    // Проверяем, виден ли элемент полностью
-    const isFullyVisible =
-      elementRect.left >= containerRect.left &&
-      elementRect.right <= containerRect.right;
-
-    if (!isFullyVisible) {
-      // Скроллим элемент в центр
-      element.scrollIntoView({
-        behavior: 'smooth',
-        block: 'nearest',
-        inline: 'center',
-      });
-    }
-  };
 
   return (
     <div style={{ overflowX: 'hidden' }}>
@@ -140,49 +296,87 @@ const PortfolioScreen: React.FC = () => {
             minHeight={true}
           />
         </div>
-        <div style={{ overflowX: 'hidden' }}>
+        <div>
           <div className="portfolio-logo-container">
-            {images.map((el, index) => (
-              <img
-                onMouseMove={() => setActiveLogo(index)}
-                onMouseLeave={() => setActiveLogo(null)}
-                key={index}
-                style={{
-                  ...el.size,
-                  opacity:
-                    activeLogo !== null && activeLogo !== index ? 0.6 : 1, // Устанавливаем opacity для всех, кроме активного
-                }}
-                src={el.icon}
-                alt=""
-                className="protfolio-logo"
-                onClick={() => handleIconClick(index)}
-              />
-            ))}
+            {images.map(
+              (el, index) =>
+                el.icon && (
+                  <img
+                    onMouseMove={() => setActiveLogo(index)}
+                    onMouseLeave={() => setActiveLogo(null)}
+                    key={index}
+                    style={{
+                      ...el.size,
+                      opacity:
+                        activeLogo !== null && activeLogo !== index ? 0.6 : 1,
+                      marginBottom: 16
+                    }}
+                    src={el.icon}
+                    alt=""
+                    className="protfolio-logo"
+                    onClick={() => {
+                      handleIconClick(index === 0 ? index : index - 1);
+                    }}
+                  />
+                ),
+            )}
           </div>
 
-          <div
-            className="portfolioImgWrapper"
-            ref={scrollContainerRef}
-            onWheel={handleWheel}
-          >
-            {images.map((el, index) => (
-              <div
-                key={index}
-                ref={ref => (imageRefs.current[index] = ref!)} // Связываем реф с элементом
-                className="portfolio-image-div"
-                data-index={index} // Добавляем атрибут data-index
-                onClick={() => handleElementClick(index)} // Добавляем обработчик клика
-              >
-                <div style={{ position: 'relative', marginBottom: '8px' }}>
-                  <img src={el.uri} alt="" className="portfolio-image" />
-                  <div className="portfolio-opacity">
-                    <Link style={{ textDecoration: 'none' }} to={el.screen}>
-                      <span className="portfolioElemImgAnimText">Подробнее</span>
-                    </Link>
+          <div className="portfolio-big-container">
+
+
+            <Swiper
+              slidesPerView={2.5}
+              freemode
+              spaceBetween={30}
+              loop={true}
+              modules={[Mousewheel, FreeMode]}
+              className="mySwiper"
+              onSwiper={(swiper: any) => (swiperRef.current = swiper)}
+              freeMode={true}
+              freeModeMomentum={true}
+              mousewheel={true}
+            >
+              {images.map((el, index) => (
+                <SwiperSlide className="portfolio-image-div" key={index}>
+                  <div
+                    ref={ref => (imageRefs.current[index] = ref!)}
+                    data-index={index}
+                  >
+                    <div style={{ position: 'relative' }}>
+                      <img src={el.uri} alt="" className="portfolio-image" />
+                      <div className="portfolio-opacity">
+                        <Link style={{ textDecoration: 'none' }} to={el.screen}>
+                          <span className="portfolioElemImgAnimText">
+                            Подробнее
+                          </span>
+                        </Link>
+                      </div>
+                    </div>
+                    <p style={{ marginTop: 0 }} className="H2">
+                      {el.title}
+                    </p>
                   </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+          <div className="portfolio-min-container">
+            {images2.map((el, index) => (
+              <Link
+                to={el.screen}
+                key={index}
+                data-index={index}
+                style={{ textDecoration: 'none' }}
+              >
+                <div style={{ position: 'relative' }}>
+                  <img src={el.uri} alt="" className="portfolio-image" />
                 </div>
-                <p className="H2">{el.title}</p>
-              </div>
+                <p style={{ marginTop: 0 }} className="H2">
+                  {el.title}
+                </p>
+                <div style={{ marginBottom: '40px' }}/>
+              </Link>
             ))}
           </div>
         </div>
